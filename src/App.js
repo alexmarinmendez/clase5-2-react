927262b8bead2464e6350ece27e01599c7f7b53b
Alex Marin Mendez <alexmarinmendez@gmail.com>
2021-11-02 23:50:30 -0500

    Initial React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Video from './Video';
+import {data} from './data';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+      data.map((video, index) => 
+        <Video
+          key={index}
+          title={video.title} 
+          dateAdded={video.dateAdded}
+          channel={video.channel}
+          thumbnail={video.thumbnail}
+          description={video.description} /> )
+    }
+    </>
   );
 }
 
---
+++ src/Video.js
@@ -0,0 +1,18 @@
+
+const Video = ({title, dateAdded, channel, thumbnail, description}) => {
+        return (
+            <div className="video-container">
+                <div className="video-image">
+                  <img src={thumbnail} alt={title} />
+                </div>
+                <div className="video-info">
+                  <h3>{title}</h3>
+                  <p>Upload date: {dateAdded}</p>
+                  <h4>Channel's Title: {channel}</h4>
+                  <p>{description}</p>
+                </div>
+            </div>
+        );
+    }
+
+export default Video;
